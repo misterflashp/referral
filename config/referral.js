345cd7a03f5eea668bc1a9935c7eb3e11653900d
Tony Stark <ironman0x7b2@protonmail.com>
2018-08-06 15:08:24 +0530

    Added can claim and claim period

--- config/referral.js
+++ config/referral.js
@@ -8,5 +8,6 @@ module.exports = {
     50: 200 * Math.pow(10, 8),
   },
   REF_BONUS: 100 * Math.pow(10, 8),
-  COINBASE_PRIVATE_KEY: ''
+  CLAIM_PERIOD: 21 * 24 * 60 * 60 * 1000,
+  COINBASE_PRIVATE_KEY: '',
 };
